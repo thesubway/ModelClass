ae50a5078dbfe922636f34e79f74ed7ef7a975f4
Daniel Hoang <thesubway@gmail.com>
2014-07-28 13:22:04 -0700

    Initial Commit

---
+++ ModelClass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModelClass
+//
+//  Created by Dan Hoang on 7/28/14.
+//  Copyright (c) 2014 Dan Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
